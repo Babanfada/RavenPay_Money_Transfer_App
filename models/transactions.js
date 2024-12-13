4b61334930cdfc69ed55c56f4338f78480d81bef
Babanfada <apatapiti@protonmail.com>
2024-12-13 16:00:19 +0100

    commit

--- models/transactions.js
+++ models/transactions.js
@@ -1,18 +1,6 @@
 const db = require("./db");
 
 // Query all transactions or filter by type
-// const findTransactionsByUserId = async (user_id, type = null) => {
-//   const query = `
-//     SELECT * FROM transactions
-//     WHERE user_id = ?
-//     ${type ? "AND type = ?" : ""}
-//     ORDER BY created_at DESC;
-//   `;
-
-//   const params = type ? [user_id, type] : [user_id];
-//   const [results] = await db.execute(query, params);
-//   return results;
-// };
 const findTransactionsByUserId = async (user_id, type = null) => {
   const query = `
     SELECT * FROM transactions
@@ -22,8 +10,8 @@ const findTransactionsByUserId = async (user_id, type = null) => {
   `;
 
   const params = type ? [user_id, type] : [user_id];
-  const results = await db.raw(query, params); // Knex uses db.raw for raw SQL queries
-  return results[0]; // Knex raw query results are returned in an array
+  const results = await db.raw(query, params); 
+  return results[0]; 
 };
 
 module.exports = { findTransactionsByUserId };
